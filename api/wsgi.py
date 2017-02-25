ec3ad04af8990847c2679dd02ba235e7408cd8ac
Benjamin117 <bluefox_117@hotmail.com>
2017-02-25 21:18:37 +1100

    api changes

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,6 +1,4 @@
-from app import app
+from LootGenapp import app
 
 if __name__ == "__main__":
-def appplication(environ,start_response):
-	start_response('200 OK',[('Content-Type','text/html')])
     app.run()
