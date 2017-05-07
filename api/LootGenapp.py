1aceeda6201c12c6966280176df8a5e479f5fad9
Benjamin117 <bluefox_117@hotmail.com>
2017-05-07 17:29:17 +1000

    added inventories feature

--- api/LootGenapp.py
+++ api/LootGenapp.py
@@ -2,9 +2,16 @@ from flask import *
 import flask
 import random
 import json
-from LootGenutils import *
+import pandas as pd
+from io import BytesIO
+import csv
 import os
+import re
+import uuid
+import xlsxwriter
+from datetime import datetime
 from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -45,6 +52,7 @@ def initialise_item(base_item,itemtype):
     base_item['sperkrarity'] = 'none'
     base_item['pperkvalue'] = '0 GP'
     base_item['sperkvalue'] = '0 GP'
+    base_item['uid'] = str(uuid.uuid4())
     base_item['itemtype'] = itemtype
     if 'Special Properties' in base_item:
         if type(base_item['Special Properties']) is not str:
@@ -138,6 +146,24 @@ def format_item(base_item):
         base_item['totalvalue'] = str(
             int(base_item['Base Value'].split()[0]) + int(base_item['pperkvalue'].split()[0]) + int(
                 base_item['sperkvalue'].split()[0])) + " GP"
+	
+	#calc overall rarity
+
+    base_item['propclass'] = 'None'
+
+    if base_item['pperkrarity'] == 'none' or base_item['sperkrarity'] == 'none':
+        base_item['perkclass']='none' #Black
+    if base_item['pperkrarity'] == 'Common perks' or base_item['sperkrarity'] == 'Common perks':
+        base_item['perkclass']='Common' #Green
+    if base_item['pperkrarity'] == 'Uncommon perks' or base_item['sperkrarity'] == 'Uncommon perks':
+        base_item['perkclass']='Uncommon' #Blue
+    if base_item['pperkrarity'] == 'Rare perks' or base_item['sperkrarity'] == 'Rare perks':
+        base_item['perkclass']='Rare' #Red
+    if base_item['pperkrarity'] == 'Very rare perks' or base_item['sperkrarity'] == 'Very rare perks':
+        base_item['perkclass']='Vrare' #Purple
+    if base_item['pperkrarity'] == 'Legendary perks' or base_item['sperkrarity'] == 'Legendary perks':
+        base_item['perkclass']='Legendary'
+        base_item['propclass']='Legendary'# Gold
 
     return json.dumps(base_item)
 
@@ -204,6 +230,87 @@ def roll_for_armour():
                     mimetype="application/json")
     return (resp)
 
+@app.route('/updateinventory')
+def updateinv():
+    newitem = request.args.get('item')
+    key = request.remote_addr
+    directory = os.path.join(SITE_ROOT, "static", "Inventories/"+str(key))
+    filepath = directory + '/inventory.json'
+    inventory = []
+    inventory.append(json.loads(newitem))
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    if os.path.isfile(filepath):
+        existing = json.load(open(filepath))
+        for item in existing:
+            inventory.append(item)
+
+    with open(filepath, 'w') as outfile:
+        json.dump(inventory, outfile,ensure_ascii=False)
+
+    return "200 OK"
+
+@app.route('/clearinventory')
+def clearinv():
+    key = request.remote_addr
+    directory = os.path.join(SITE_ROOT, "static", "Inventories/" + str(key))
+    filepath = directory + '/inventory.json'
+
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    return "200 OK"
+
+@app.route('/getinventory')
+def getinv():
+    key = request.remote_addr
+    directory = os.path.join(SITE_ROOT, "static", "Inventories/" + str(key))
+    filepath = directory + '/inventory.json'
+
+    if os.path.isfile(filepath):
+        return json.dumps(json.load(open(filepath)))
+
+    return json.dumps([])
+
+@app.route('/downloadinventory')
+def dl_session():
+    key = request.remote_addr
+    directory = os.path.join(SITE_ROOT, "static", "Inventories/" + str(key))
+    filepath = directory + '/inventory.json'
+
+    if not os.path.isfile(filepath):
+        return "Nothing in Inventory"
+
+    # create an output stream
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+
+    items_json=json.load(open(filepath))
+    weapons_json = []
+    armour_json = []
+    for item in items_json:
+        if item['itemtype']=='Weapon':
+            weapons_json.append(item)
+        if item['itemtype']=='Armour':
+            armour_json.append(item)
+
+    armour = pd.DataFrame(armour_json)
+    armour.to_excel(writer, startrow=0, merge_cells=False, sheet_name="armour")
+
+    weapons = pd.DataFrame(weapons_json)
+    weapons.to_excel(writer, startrow=0, merge_cells=False, sheet_name="weapons")
+
+    writer.close()
+    output.seek(0)
+
+    fileoutname = "Inventory_"+str(datetime.now())+"xlsx"
+
+    return send_file(output, attachment_filename=fileoutname, as_attachment=True)
+
+
+
+
+
 
 @app.route("/")
 def home():
@@ -211,5 +318,5 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', port=81, debug=False)
+    app.run(host= '0.0.0.0', port=7000, debug=False)
     #roll_for_armour('hard',2)
