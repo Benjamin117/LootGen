7ae79a44b340901f45ef785b9d5edc63ea1abdb6
Benjamin117 <bluefox_117@hotmail.com>
2017-06-25 15:10:27 +1000

    API running Gunicorn in docker

---
+++ api/app/config.py
@@ -0,0 +1,8 @@
+bind = "0.0.0.0:80"
+chdir = "/app"
+loglevel = "INFO"
+workers = "2"
+worker_class = "gevent"
+reload = True
+errorlog = "-"
+accesslog = "-"
--- api/app/wsgi.py
+++ api/app/wsgi.py
@@ -1,4 +1,5 @@
 from LootGenapp import app
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host ='0.0.0.0',port=80)
